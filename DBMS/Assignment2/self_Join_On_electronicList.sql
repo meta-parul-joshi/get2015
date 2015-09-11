f7e29e07493e8b2f3008b9a2c55a93548d37e151
Parul Joshi <parul.joshi@metacube.com>
2015-09-11 18:12:17 +0530

    Completed

--- DBMS/Assignment2/self_Join_On_electronicList.sql
+++
@@ -1,6 +0,0 @@
-use electronicList;
-
-SELECT c.id as categoryId, c.name as categoryName, IFNULL(parent1.name,"Top category") as parentName
-FROM categories as c LEFT JOIN categories as parent1 ON parent1.id = c.parentId;
-
-SELECT name FROM categories where parentId is null ;
