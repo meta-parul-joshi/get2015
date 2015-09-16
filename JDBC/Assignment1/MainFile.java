cfe6c2b3cf048709e7959e23b551285a27b6f476
meta-parul-joshi <parul.joshi@metacube.com>
2015-09-16 17:18:55 +0530

    Update MainFile.java

--- JDBC/Assignment1/MainFile.java
+++ JDBC/Assignment1/MainFile.java
@@ -7,48 +7,70 @@ public class MainFile
 	public static void main(String[] args) throws ParseException 
 	{
 		System.out.println("Library Information System");
+		
+		/*Initialize object of class to 
+		 * access methods of class.*/
 		LibraryInformationSystemHelper objectOfInformationSystemHelper = new LibraryInformationSystemHelper();
 		while(true)
 		{
-			System.out.println("\n1. Want to know books publishe by author ");
+			System.out.println("\n1. Want to know books published by author ");
 			System.out.println("2. Want to issue book");
+			System.out.println("3. Remove books from library that aer not issued since last one year");
+			System.out.println("4.Exit");
+			
+			/*Input from user.*/
 			int choice = Utility.getIntegerValue("\nEnter your choice :");
 			switch(choice)
 			{
+						/*Take input author name from user and displays books written by author.*/
 				case 1 :String authorName = Utility.getStringInput("Enter author name");
-						boolean flag = objectOfInformationSystemHelper.checkAuthorExist(authorName);
-						if(!flag)
-						{
-							System.out.println("There is no book of this author in library");
-						}
+						List<Title> titleList=new ArrayList<Title>();
+						titleList = objectOfInformationSystemHelper.booksPublishedByAuthor(authorName);
+						printListOFBooks(titleList);
 						break;
-				
+						
+						/*Take book name and member name as input and display whether book issued or not*/
 				case 2:String bookName = Utility.getStringInput("Enter name of book to be isssued");
-						flag = objectOfInformationSystemHelper.checkBookInLibrary(bookName);
-						if(flag)
-						{	
-							System.out.println("\nEnter member details :");
-							Members objMembers = new Members();			
-							String name = Utility.getStringInput("Enter name");
-							objMembers.setMemberName(name);
-							String addressLine1 = Utility.getStringInput("Enter address line 1");
-							objMembers.setAddressLine1(addressLine1);
-							String addressLine2 = Utility.getStringInput("Enter address line 2");
-							objMembers.setAddressLine2(addressLine2);
-							Character category = Utility.getCharacterInput("Enter category");
-							objMembers.setCategory(category);
-							objectOfInformationSystemHelper.bookIssuedOrNot(objMembers, bookName);
+						System.out.println("\nEnter member details :");		
+						String name = Utility.getStringInput("Enter member name");
+						boolean flag = objectOfInformationSystemHelper.bookIssuedOrNot( bookName,name);	
+						if(flag == true)
+						{
 							System.out.println("Book issued successfully");
 						}
 						else
 						{
-							System.out.println("Book is not available in library");
+							System.out.println("Book not issued");
 						}	
 						break;
 						
-					default : System.out.println("Wrong Choice...!!");
+						/*Remove books from library that aer not issued since last one yea*/
+				case 3: int numberOfBooksDeleted = objectOfInformationSystemHelper.removeBooksFromThatAreNotIssuedSinceLasttYear();
+						System.out.println("Number of books deleted - " + numberOfBooksDeleted);
+						break;
+						
+						/*Exit.*/
+				case 4 : System.out.println("System exited ");
+						System.exit(0);
+						break;
+						
+						/*Default case.*/
+				default : System.out.println("Wrong Choice...!!");
 							break;
 			}
+		}
+	}
+	
+	/*Function to print number of books written by author.
+	 * It takes titleList as key parameter.
+	 * */
+	static void printListOFBooks(List<Title> titleList)
+	{
+		System.out.println("Title id\tTitle name\tSubject id\tPublisher id");
+		for(Title s : titleList)
+		{
+			System.out.println(s.getTitleId()+"\t\t"+s.getTitleName()
+					+"\t"+s.getSubjectId()+"\t"+s.getPublisherId());
 		}
 	}
 }
