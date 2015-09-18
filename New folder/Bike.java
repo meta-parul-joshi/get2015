84ef9b49efd37593d7763ff30b509f1046b90fb2
meta-parul-joshi <parul.joshi@metacube.com>
2015-09-18 15:00:10 +0530

    Update Bike.java

--- New folder/Bike.java
+++ New folder/Bike.java
@@ -1,11 +1,52 @@
 //class for unique features of bike
-public class Bike extends Vehicle {
-	String standAlarm;
-	String steppedSeat;
+public class Bike extends Vehicle 
+{
+	boolean isSelfStart ;
+	float price;
+	Bike(String make, String model) 
+	{
+		super(make,model);
+	}
+	
+	public boolean isSelfStart() {
+		return isSelfStart;
+	}
 
-	// constructor to set model and make
-	Bike(String make, String model) {
-		this.make = make;
-		this.model = model;
+	public void setSelfStart(boolean isSelfStart) {
+		this.isSelfStart = isSelfStart;
+	}
+
+	public float getPrice() {
+		return price;
+	}
+
+	public void setPrice(float price) {
+		this.price = price;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = super.hashCode();
+		result = prime * result + (isSelfStart ? 1231 : 1237);
+		result = prime * result + Float.floatToIntBits(price);
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!super.equals(obj))
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Bike other = (Bike) obj;
+		if (isSelfStart != other.isSelfStart)
+			return false;
+		if (Float.floatToIntBits(price) != Float.floatToIntBits(other.price))
+			return false;
+		return true;
 	}
 }
+
