5ffb77dab119eee6d28b2adfc736016896cdc01f
meta-parul-joshi <parul.joshi@metacube.com>
2015-09-18 15:00:57 +0530

    Update Car.java

--- New folder/Car.java
+++ New folder/Car.java
@@ -1,11 +1,66 @@
 //class for unique features of car
-public class Car extends Vehicle {
-	int airBags;
-	int airConditioner;
+public class Car extends Vehicle 
+{
+	boolean isAirBags;
+	boolean isAirConditioner;
+	boolean isPowerSteering;
 
 	// constructor to set model and make
-	Car(String make, String model) {
-		this.make = make;
-		this.model = model;
+	Car(String make, String model)
+	{
+		super(make,model);
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = super.hashCode();
+		result = prime * result + (isAirBags ? 1231 : 1237);
+		result = prime * result + (isAirConditioner ? 1231 : 1237);
+		result = prime * result + (isPowerSteering ? 1231 : 1237);
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!super.equals(obj))
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Car other = (Car) obj;
+		if (isAirBags != other.isAirBags)
+			return false;
+		if (isAirConditioner != other.isAirConditioner)
+			return false;
+		if (isPowerSteering != other.isPowerSteering)
+			return false;
+		return true;
+	}
+
+	public boolean isAirBags() {
+		return isAirBags;
+	}
+
+	public void setAirBags(boolean isAirBags) {
+		this.isAirBags = isAirBags;
+	}
+
+	public boolean isAirConditioner() {
+		return isAirConditioner;
+	}
+
+	public void setAirConditioner(boolean isAirConditioner) {
+		this.isAirConditioner = isAirConditioner;
+	}
+
+	public boolean isPowerSteering() {
+		return isPowerSteering;
+	}
+
+	public void setPowerSteering(boolean isPowerSteering) {
+		this.isPowerSteering = isPowerSteering;
 	}
 }
+
