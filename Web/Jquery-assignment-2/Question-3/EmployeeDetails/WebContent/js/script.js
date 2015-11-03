e0db31f7b35c9ff96537c7b274e65736eb5ccd4d
meta-parul-joshi <parul.joshi@metacube.com>
2015-11-03 11:20:20 +0530

    Update script.js

--- Web/Jquery-assignment-2/Question-3/EmployeeDetails/WebContent/js/script.js
+++ Web/Jquery-assignment-2/Question-3/EmployeeDetails/WebContent/js/script.js
@@ -8,27 +8,14 @@ function init() {
 }
 
 function loadInfo() {
-	var xhttp;
-	if (window.XMLHttpRequest)
-	{
-	// code for modern browsers
-		xhttp = new XMLHttpRequest();
-	}
-	else 
-	{
-	// code for IE6, IE5
-		xhttp = new ActiveXObject("Microsoft.XMLHTTP");
-	}
-
 	var id = $("#employeeName").val();
-	xhttp.open("POST", "EmployeeDetail?id=" + id, true);
-	xhttp.send();
-
-	xhttp.onreadystatechange = function() 
-	{
-		if (xhttp.readyState == 4 && xhttp.status == 200) 
-		{
-			$('#demo').html(xhttp.responseText);
-		}
-	}
+	$.ajax({
+        type : "POST",
+        url : "EmployeeDetail",
+        data :  {id : id},
+        datatype : "html",
+        success : function(data) {
+            $("#demo").html(data);
+        }
+    });
 }
