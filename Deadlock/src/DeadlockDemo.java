40ab67a059d4c5e1fe87095a60db8ab7bb38bae9
Parul <parul.joshi@metacube.com>
2015-12-21 18:37:47 +0530

    completed

---
+++ Deadlock/src/DeadlockDemo.java
@@ -0,0 +1,46 @@
+public class DeadlockDemo {
+
+	public static Object resource1 = new Object();
+	public static Object resource2 = new Object();
+
+	private static class ThreadDemo1 extends Thread {
+		public void run() {
+			synchronized (resource1) {
+				System.out.println("Thread 1: Holding resource 1...");
+				try {
+					Thread.sleep(10);
+				} catch (InterruptedException e) {
+				}
+				System.out.println("Thread 1: Waiting for resource 2...");
+				synchronized (resource2) {
+					System.out.println("Thread 1: Holding resource 1 & 2...");
+				}
+			}
+		}
+	}
+
+	private static class ThreadDemo2 extends Thread {
+		public void run() {
+			synchronized (resource2) {
+				System.out.println("Thread 2: Holding resource 2...");
+				try {
+					Thread.sleep(10);
+				} catch (InterruptedException e) {
+				}
+				System.out.println("Thread 2: Waiting for resource 1...");
+				synchronized (resource1) {
+					System.out.println("Thread 2: Holding resource 1 & 2...");
+				}
+			}
+		}
+	}
+
+	public static void main(String args[]) {
+
+		ThreadDemo1 T1 = new ThreadDemo1();
+		ThreadDemo2 T2 = new ThreadDemo2();
+		T1.start();
+		T2.start();
+	}
+
+}
